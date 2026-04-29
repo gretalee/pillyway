57ce830aaadce654d4236f2b0b24767a88d898bb
Hendrike Heydenreich <mail@gretalee.de>
2026-04-29 18:39:59 +0200

    rename config file

--- apps/frontend/next.config.ts
+++ apps/frontend/next.config.ts
@@ -1,8 +1,8 @@
-import createNextIntlPlugin from 'next-intl/plugin';
-import path from 'path';
-import type { NextConfig } from 'next';
+import createNextIntlPlugin from "next-intl/plugin";
+import path from "path";
+import type { NextConfig } from "next";
 
-const withNextIntl = createNextIntlPlugin('./i18n/request.ts');
+const withNextIntl = createNextIntlPlugin("./i18n/geti18nConfig.ts");
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.resolve(import.meta.dirname),
