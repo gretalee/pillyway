2ae8aca87ace54c1c91f400fa530e26e71b0b30b
Hendrike Heydenreich <mail@gretalee.de>
2026-05-21 23:04:05 +0200

    footer styles

--- apps/frontend/app/components/layout/Footer.tsx
+++ apps/frontend/app/components/layout/Footer.tsx
@@ -8,7 +8,7 @@ export async function Footer({ className }: { className?: string }) {
   return (
     <footer className={cn('w-full bg-black', className)}>
       <div className="mx-auto flex justify-end max-w-7xl items-center justify-between px-4 sm:px-6 lg:px-8 py-2 text-white antialiased">
-        <ul className="flex gap-4 ">
+        <ul className="flex lg:gap-4 flex-col lg:flex-row">
           <li>
             <Link href="/imprint" className="text-sm xl:text-base">
               {t('imprint')}
